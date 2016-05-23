7cb7f894c5e4547bead0a442e40a5c3ab3994d66
Thomas Lisankie <link285@gmail.com>
2016-05-23 18:43:14 -0400

    -

--- Prhymer/FindWordsViewController.swift
+++ Prhymer/FindWordsViewController.swift
@@ -18,13 +18,32 @@ class FindWordsViewController: UIViewController {
         
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate;
         
+        /*
+         This process:
+         - needs to be random
+         - selects six words at most during an iteration
+         
+         steps:
+         - selects a random number between 0 and the size of the dictionary
+         - checks to see if this index has already been examined
+         - if so, it repeats this process
+         - if it hasn't:
+            - adds this number to another dictionary [Int: Bool]
+            - finds the rhyme percentile between the initial word and the word at the index being found
+            - if the RP is above 0.65, it gets added to the queue for well-rhyming ("green") words
+            - otherwise, if the RP is above 0.45 but below 0.65, it gets added to the queue for mildly-rhyming ("yellow") words
+            - otherwise, if it's below 0.45, it's not added to anything
+         - three words from each queue are popped and listed
+         -
+         */
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //Looks for single or multiple taps.
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(FindWordsViewController.dismissKeyboard));
         view.addGestureRecognizer(tap)
     }
     
