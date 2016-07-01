56fd5807a7a5eb0ca4504b686181239ec524db9c
Thomas Lisankie <link285@gmail.com>
2016-06-30 23:53:56 -0400

    -

--- Piece.swift
+++ Piece.swift
@@ -8,10 +8,11 @@
 
 import Foundation
 
-class Piece {
+@objc(Piece)
+class Piece : NSObject, NSCoding {
     
-    var title: String;
-    var content: String;
+    var title: String?;
+    var content: String?;
     //date of creation
     
     init(title: String){
@@ -19,6 +20,38 @@ class Piece {
         self.title = title;
         content = "";
     
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        
+        if let title = aDecoder.decodeObjectForKey("title") as? String {
+            
+            self.title = title;
+            
+        }
+        
+        if let content = aDecoder.decodeObjectForKey("content") as? String {
+            
+            self.content = content;
+            
+        }
+        
+    }
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        
+        if let title = self.title {
+            
+            aCoder.encodeObject(title, forKey: "title")
+            
+        }
+        
+        if let content = self.content {
+            
+            aCoder.encodeObject(content, forKey: "content")
+            
+        }
+        
     }
     
 }
